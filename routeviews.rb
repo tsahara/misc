0a9085de250a52c10b477dc34d950c82287174fa
Tomoyuki Sahara <sahara@caddr.net>
2017-09-01 23:39:22 +0900

    print number of asn.

--- routeviews.rb
+++ routeviews.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 def check_loop(paths)
-  lp = false
+  lp = []
   len = paths.size
   paths.each_index { |i|
     as = paths[i]
@@ -10,7 +10,7 @@ def check_loop(paths)
     while j < len
       if paths[j] == as
         puts "#{as} <- #{paths}"
-        lp = true
+        lp << as
       end
       j += 1
     end
@@ -20,25 +20,39 @@ end
 
 routes = loop_routes = 0
 
+$first_prefix_only = true
+prefix_hash = {}
+asn = {}
+
 5.times { gets }
 while l = gets
-  routes += 1
   a = l.split
-  if a[0] == "*"
-    a.shift(6)
-    if check_loop(a)
-      loop_routes += 1
-    end
-    #    Network            Next Hop            Metric LocPrf Weight Path
-    # *  1.177.160.0/22     185.44.116.1             0      0      0 47872 15412 9848 38091 38091 38091 38091 38091 38091 9845 9697 i
 
-  else
-    puts "first column is not * : #{l.strip}"
-    exit
+  raise "first column is not * : #{l.strip}" if a[0] != "*"
+
+  next if prefix_hash.has_key? a[1]
+  #$stdout.write "\r#{a[1]}"
+  #$stdout.flush
+  prefix_hash[a[1]] = true
+
+  a.shift(6)
+  a.each { |as| asn[as] ||= false }
+  as_list = check_loop(a)
+  if as_list.size > 0
+    loop_routes += 1
+    as_list.each { |as| asn[as] = true }
   end
+  #    Network            Next Hop            Metric LocPrf Weight Path
+  # *  1.177.160.0/22     185.44.116.1             0      0      0 47872 15412 9848 38091 38091 38091 38091 38091 38091 9845 9697 i
 
-  if routes % 1000 == 0
+  routes += 1
+  if routes % 1000000 == 0
     ratio = loop_routes.to_f / routes
     puts "#{loop_routes} / #{routes} routes = #{ratio}"
   end
 end
+
+puts "Number of ASN = #{asn.size}"
+
+looped = asn.select { |k,v| v }
+puts "Looped ASN: #{looped.keys.size}"
